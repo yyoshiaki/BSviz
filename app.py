04d95192296f909a68b006c930e98ca9d0c95b4f
yyoshiaki <y.yasumizu@gmail.com>
2021-06-02 09:55:56 +0900

    port

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def test():
     return render_template("test.html", figs=figs)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5020, debug=True)
+    app.run(host='0.0.0.0', debug=True)
